50f22ced6d9e830d4eb8da099b79bac7bb645766
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2024-09-10 22:35:00 +0700

    Set up new project

---
+++ admin/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="text-red-500">
+      Hello
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-red-500">Hi</div>;
+};
+
+export default App;
