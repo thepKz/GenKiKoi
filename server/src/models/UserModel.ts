358cd6437646db45912457177c6b125b41a54a1f
troidatoi <honghanh04122004@gmail.com>
2024-09-29 16:02:10 +0700

    updateUserModel

--- server/src/models/UserModel.ts
+++ server/src/models/UserModel.ts
@@ -4,6 +4,7 @@ interface IUser {
   username: string;
   email: string;
   password: string;
+  fullname: string;
   photoUrl?: string;
   role: "customer" | "staff" | "veterinarian" | "manager";
 }
@@ -13,6 +14,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    fullname: { type: String, required: true },
     photoUrl: { type: String, default: "" },
     role: {
       type: String,
