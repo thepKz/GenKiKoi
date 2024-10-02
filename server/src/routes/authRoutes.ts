fb084b215e300fabf73da26d270740778cc36cb2
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2024-10-02 10:45:52 +0700

    Check exist email and username

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  checkEmail,
+  checkUsername,
   login,
   loginWithGoogle,
   register,
@@ -10,5 +12,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/login-google", loginWithGoogle);
+router.post("/check-username", checkUsername);
+router.post("/check-email", checkEmail);
 
 export default router;
