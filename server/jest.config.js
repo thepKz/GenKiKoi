b511d12a6939a2eb13a282b5ca918d5f2fff1e53
thepKz <thepmtse181568@fpt.edu.vn>
2024-10-13 06:54:04 +0700

    setup JEST

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: 'node',
   testEnvironmentOptions: {
     NODE_ENV: 'test',
   },
@@ -13,7 +12,18 @@ module.exports = {
     "/__tests__/api/"
   ],
   restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.ts', 'tests'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'src/config',
+    'src/app.ts',
+    'tests'
+  ],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
   globalTeardown: './jest.teardown.js',
+
+  // Thêm reporters để xuất kết quả test ra file JSON
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: './', outputName: 'jest-results.json' }]
+  ],
 };
