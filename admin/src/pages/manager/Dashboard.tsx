9bf54cf40769667ced097cc08007b55d9459d915
Ánh Hồng <lehong3568@gmail.com>
2024-11-01 16:15:20 +0700

    remove: Remove unusable state

--- admin/src/pages/manager/Dashboard.tsx
+++ admin/src/pages/manager/Dashboard.tsx
@@ -13,7 +13,6 @@ const Dashboard = () => {
   const [totalEarning, setTotalEarning] = useState<number>(0);
   const [totalBooking, setTotalBooking] = useState<number>(0);
   const [totalCustomers, setTotalCustomers] = useState<number>(0);
-  const [topServices, setTopServices] = useState([null]);
 
   useEffect(() => {
     const getData = async () => {
