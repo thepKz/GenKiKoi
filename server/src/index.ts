37794fe3cd38e8610ebee2c042628632a66c2d68
Hồng Hạnh <honghanh04122004@gmail.com>
2024-11-01 21:31:44 +0700

    add: Add scheduledTask for update status of outdate appointments

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,6 @@ import { swaggerDocument, swaggerUi } from "./swagger";
 dotenv.config();
 
 // Routes
-import { DoctorSchedule, MedicalRecord } from "./models";
 import {
   appointmentRoutes,
   authRoutes,
@@ -24,6 +23,9 @@ import {
   staffRoutes,
   userRoutes,
 } from "./routes";
+
+// SERVICES
+import { startScheduledTasks } from "./services/sheduledTasks";
 
 const app = express();
 
@@ -79,102 +81,7 @@ app.use("/api/customers", customerRoutes);
 // Export the app for testing purposes
 export { app };
 
-// Add medical record
-const addMedicalRecord = async () => {
-  try {
-    const newMedicalRecord = await MedicalRecord.create({
-      doctorId: "67015e789a7dce19f9fb0015",
-      customerId: "6711fdbea11c8327da32e5e5",
-      fishId: "6710abfd9145638f0eb79dde",
-      examType: "Khám bệnh",
-      serviceName: "Siêu âm",
-      diagnosis: "Bệnh viêm da",
-      treatment: "Sử dụng thuốc kháng sinh",
-      medicines: [
-        {
-          name: "Thuốc A",
-          quantity: 2,
-        },
-        {
-          name: "Thuốc B",
-          quantity: 1,
-        },
-      ],
-      images: [
-        "https://placehold.co/150x150?text=Demo1",
-        "https://placehold.co/150x150?text=Demo2",
-        "https://placehold.co/150x150?text=Demo3",
-        "https://placehold.co/150x150?text=Demo4",
-      ],
-    });
-    console.log(newMedicalRecord);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// addMedicalRecord()
-
-// Add medical record
-const addDoctorSchedule = async () => {
-  try {
-    const newDoctorSchedule = await DoctorSchedule.create({
-      doctorId: "67015e789a7dce19f9fb0015", // Thay thế bằng ID thực của bác sĩ
-      weekSchedule: [
-        {
-          dayOfWeek: "30/10/2024", // Thứ 7
-          slots: [
-            {
-              slotTime: "8:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-            {
-              slotTime: "9:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-            {
-              slotTime: "10:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-            {
-              slotTime: "11:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-            {
-              slotTime: "13:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-            {
-              slotTime: "14:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-            {
-              slotTime: "15:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-            {
-              slotTime: "16:00",
-              isBooked: false,
-              currentCount: 0,
-            },
-          ],
-        },
-      ],
-    });
-    console.log(newDoctorSchedule);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// addDoctorSchedule()
+startScheduledTasks();
 
 // Only start the server if this file is run directly (not imported as a module)
 if (require.main === module) {
